5c2688608b4076c0d722574cd6733abef9bebe5a
王海明 <fighting282367079@gmail.com>
2022-09-22 22:45:14 +0800

    提交外观模式

---
+++ camel-demo/camel-design-demo/camel-1-design-demo-10/src/main/java/com/camel/design/demo/aa.java
@@ -0,0 +1,14 @@
+package com.camel.design.demo;
+
+public class aa {
+
+    public static void main(String[] args) {
+
+        /**
+         * 外观类就是普通的  service  和 impl
+         * 然后controller去调用service
+         */
+        System.out.println("就是最普通的最熟知的开发模式就是外观模式");
+    }
+
+}
